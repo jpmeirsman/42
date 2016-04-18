b37d1ca72400631293bb0f2b3f4aa4b5087f179b
Jean-Paul MEIRSMAN <jean-paul@meirsman.fr>
2016-04-18 21:01:23 +0200

    version 0.0

--- fdf/gobject.c
+++ fdf/gobject.c
@@ -159,7 +159,7 @@ tVector2	negateVector2(tVector2 myVector)
 tVector2	scaleVector2(tVector2 myVector, double scale)
 {
 	tVector2	temp;
-	
+
 	temp.x = myVector.x * scale;
 	temp.y = myVector.y * scale;
 	return (temp);
@@ -179,7 +179,7 @@ double	lengthVector2(tVector2 myVector)
 	double	lengthVector2;
 
 	lengthVector2 = sqrt(myVector.x * myVector.x + myVector.y 
-		* myVector.y);
+			* myVector.y);
 	return (lengthVector2);
 }
 
@@ -275,6 +275,7 @@ double		DistanceVector2(tVector2 value1, tVector2 value2)
 	y = value1.y - value2.y;
 	return ((x * x) + (y * y));
 }
+
 char		*Vector3toString(tVector3 tv)
 {
 	char		*s;
@@ -328,7 +329,7 @@ tVector3	negateVector3(tVector3 myVector)
 tVector3	scaleVector3(tVector3 myVector, double scale)
 {
 	tVector3	temp;
-	
+
 	temp.x = myVector.x * scale;
 	temp.y = myVector.y * scale;
 	temp.z = myVector.z * scale;
@@ -340,7 +341,7 @@ bool		equalsVector3(tVector3 myVector, tVector3 other)
 	bool	test;
 
 	if ((myVector.x == other.x) && (myVector.y == other.y) &&
-		(myVector.z == other.z))
+			(myVector.z == other.z))
 		return (true);
 	return (false);
 }
@@ -397,23 +398,121 @@ tVector3	normalizeVector3(tVector3 myVector)
 	return (temp);
 }
 
-tVector3	FromArrayVector3(array, short offset)
+tVector3	FromArrayVector3(double *array, long offset)
 {
 	tVector3	temp;
+	double		a;
+	double		b;
+	double		c;
 
 	if (!offset)
 		offset = 0;
-	return (setVector3(array[offset], array[offset + 1], array[offset + 2]));
+	temp.x = array[offset];
+	temp.y = array[offset+1];
+	temp.z = array[offset+2];
+	return (temp);
 }
 
 tVector3	setVector3(double X, double Y, double Z)
 {
-	tVector3	Vector3;
+	tVector3	temp;
 
-	Vector3.x = X;
-	Vector3.y = Y;
-	Vector3.z = Z;
-	return (Vector3);
+	temp.x = X;
+	temp.y = Y;
+	temp.z = Z;
+	return (temp);
+}
+
+tVector3	ZeroVector3()
+{
+	return (setVector3(0, 0, 0));
+}
+
+tVector3	UpVector3()
+{
+	return (setVector3(0, 1.0, 0));
+}
+
+tVector3	CopyVector3(tVector3 source)
+{
+	tVector3	temp;
+
+	temp.x = source.x;
+	temp.y = source.y;
+	temp.z = source.z;
+	return (temp);
+}
+
+tVector3	TransformCoordinatesVector3(tVector3 vector, tmatrix transformation)
+{
+	double		x;
+	double		y;
+	double		z;
+	double		w;
+	tVector3	temp;
+
+	x = (vector.x * transformation.m[0]) + (vector.y * transformation.m[4]		) + (vector.z * transformation.m[8]) + transformation.m[12];
+	y = (vector.x * transformation.m[1]) + (vector.y * transformation.m[5]		) + (vector.z * transformation.m[9]) + transformation.m[13];
+	z = (vector.x * transformation.m[2]) + (vector.y * transformation.m[6]		) + (vector.z * transformation.m[10]) + transformation.m[14];
+	w = (vector.x * transformation.m[3]) + (vector.y * transformation.m[7]		) + (vector.z * transformation.m[11]) + transformation.m[15];
+	temp.x = x / w;
+	temp.y = y / w;
+	temp.z = z / w;
+	return (temp);
+}
+
+tVector3	TransformNormalVector3(tVector3 vector, tmatrix transformation)
+{
+	tVector3	temp;
+
+	temp.x = (vector.x * transformation.m[0]) + (vector.y * 
+			transformation.m[4]) + (vector.z * transformation.m[8]);
+	temp.y = (vector.x * transformation.m[1]) + (vector.y * 
+			transformation.m[5]) + (vector.z * transformation.m[9]);
+	temp.z = (vector.x * transformation.m[2]) + (vector.y * 
+			transformation.m[6]) + (vector.z * transformation.m[10]);
+	return (temp);
+}
+
+double		DotVector3(tVector3 left, tVector3 right)
+{
+	return (left.x * right.x + left.y * right.y + left.z * right.z);
+}
+
+tVector3	CrossVector3(tVector3 left, tVector3 right)
+{
+	tVector3	temp;
+
+	temp.x = left.y * right.z - left.z * right.y;
+	temp.y = left.z * right.x - left.x * right.z;
+	temp.z = left.x * right.y - left.y * right.x;
+	return (temp);
+}
+
+tVector3	NormalizeVector3(tVector3 vector)
+{
+	tVector3	temp;
+
+	temp = CopyVector3(vector);
+	temp = normalizeVector3(temp);
+	return (temp);
+}
+
+double		DistanceSquaredVector3(tVector3 value1, tVector3 value2)
+{
+	return (sqrt(DistanceSquaredVector3(value1, value2)));
+}
+
+double		DistanceVector3(tVector3 value1, tVector3 value2)
+{
+	double		x;
+	double		y;
+	double		z;
+
+	x = value1.x - value2.x;
+	y = value1.y - value2.y;
+	z = value1.z - value2.z;
+	return ((x * x) + (y * y) + (z * z));
 }
 
 tcam		*newcam(long posx, long posy, long posz, long targetx, long targety, long targetz)
@@ -489,10 +588,10 @@ void		printcube(tmesh *mycube)
 	printf("Nom du cube : %s\n",mycube->name);
 	for(int i = 0 ; i < 8; i++)
 		printf("Vertex n° %d : x=%f, y=%f, z=%f\n",i,mycube->vertices[i].x,
-			mycube->vertices[i].y,mycube->vertices[i].z);
+				mycube->vertices[i].y,mycube->vertices[i].z);
 	for(int i = 0 ; i < 12; i++)
 		printf("Face n° %d : A=%f, B=%f, C=%f\n",i,mycube->faces[i].A,
-			mycube->faces[i].B,mycube->faces[i].C);
+				mycube->faces[i].B,mycube->faces[i].C);
 }
 
 bool		MatrixisIdentity(tmatrix matrix)
@@ -500,24 +599,136 @@ bool		MatrixisIdentity(tmatrix matrix)
 	char	index;
 
 	index = true;
-	if (matrix.m[0] != 1 || matrix.m[5] != 1 || matrix.m[10] != 1 || matrix.m[15] != 1 )
+	if ((matrix.m[0] != 1.0) || (matrix.m[5] != 1.0) || 
+			(matrix.m[10] != 1.0) || (matrix.m[15] != 1.0))
 		return (false);
-	if (matrix.m[1] != 0 || matrix.m[2] != 0 || matrix.m[3] != 0 || matrix.m[4] != 0 || matrix.m[6] != 0 || matrix.m[7] != 0 || matrix.m[8] != 0 || matrix.m[9] != 0 || matrix.m[11] != 0 || matrix.m[12] != 0 || matrix.m[13] != 0 || matrix.m[14] != 0 )
+	if (matrix.m[1] != 0.0 || matrix.m[2] != 0.0 || matrix.m[3] != 0.0 || matrix.m[4] != 0.0 || matrix.m[6] != 0.0 || matrix.m[7] != 0.0 || matrix.m[8] != 0.0 || matrix.m[9] != 0.0 || matrix.m[11] != 0.0 || matrix.m[12] != 0.0 || matrix.m[13] != 0.0 || matrix.m[14] != 0.0 )
 		return false;
 	return (true);
 }
 
+double		determinantMatrix(tmatrix matrix)
+{
+	double temp1;
+	double temp2;
+	double temp3;
+	double temp4;
+	double temp5;
+	double temp6;
+
+	temp1 = (matrix.m[10] * matrix.m[15]) - (matrix.m[11] * matrix.m[14]);
+	temp2 = (matrix.m[9] * matrix.m[15]) - (matrix.m[11] * matrix.m[13]);
+	temp3 = (matrix.m[9] * matrix.m[14]) - (matrix.m[10] * matrix.m[13]);
+	temp4 = (matrix.m[8] * matrix.m[15]) - (matrix.m[11] * matrix.m[12]);
+	temp5 = (matrix.m[8] * matrix.m[14]) - (matrix.m[10] * matrix.m[12]);
+	temp6 = (matrix.m[8] * matrix.m[13]) - (matrix.m[9] * matrix.m[12]);
+	return ((((matrix.m[0] * (((matrix.m[5] * temp1) - (matrix.m[6] * temp2)) 
+							+ (matrix.m[7] * temp3))) - (matrix.m[1] * (((matrix.m[4] * temp1)
+								- (matrix.m[6] * temp4)) + (matrix.m[7] * temp5)))) + (matrix.m[2]
+					* (((matrix.m[4] * temp2) - (matrix.m[5] * temp4)) + (matrix.m[7]
+							* temp6)))) - (matrix.m[3] * (((matrix.m[4] * temp3)
+							* - (matrix.m[5] * temp5)) + (matrix.m[6] * temp6))));
+}
+
+tmatrix		toArrayMatrix(tmatrix matrix)
+{
+	return (matrix);
+}
+
+tmatrix		invertMatrix(tmatrix matrix)
+{
+	double l1 = matrix.m[0];
+	double l2 = matrix.m[1];
+	double l3 = matrix.m[2];
+	double l4 = matrix.m[3];
+	double l5 = matrix.m[4];
+	double l6 = matrix.m[5];
+	double l7 = matrix.m[6];
+	double l8 = matrix.m[7];
+	double l9 = matrix.m[8];
+	double l10 = matrix.m[9];
+	double l11 = matrix.m[10];
+	double l12 = matrix.m[11];
+	double l13 = matrix.m[12];
+	double l14 = matrix.m[13];
+	double l15 = matrix.m[14];
+	double l16 = matrix.m[15];
+	double l17 = (l11 * l16) - (l12 * l15);
+	double l18 = (l10 * l16) - (l12 * l14);
+	double l19 = (l10 * l15) - (l11 * l14);
+	double l20 = (l9 * l16) - (l12 * l13);
+	double l21 = (l9 * l15) - (l11 * l13);
+	double l22 = (l9 * l14) - (l10 * l13);
+	double l23 = ((l6 * l17) - (l7 * l18)) + (l8 * l19);
+	double l24 = -(((l5 * l17) - (l7 * l20)) + (l8 * l21));
+	double l25 = ((l5 * l18) - (l6 * l20)) + (l8 * l22);
+	double l26 = -(((l5 * l19) - (l6 * l21)) + (l7 * l22));
+	double l27 = 1.0 / ((((l1 * l23) + (l2 * l24)) + (l3 * l25)) + (l4 * l26));
+	double l28 = (l7 * l16) - (l8 * l15);
+	double l29 = (l6 * l16) - (l8 * l14);
+	double l30 = (l6 * l15) - (l7 * l14);
+	double l31 = (l5 * l16) - (l8 * l13);
+	double l32 = (l5 * l15) - (l7 * l13);
+	double l33 = (l5 * l14) - (l6 * l13);
+	double l34 = (l7 * l12) - (l8 * l11);
+	double l35 = (l6 * l12) - (l8 * l10);
+	double l36 = (l6 * l11) - (l7 * l10);
+	double l37 = (l5 * l12) - (l8 * l9);
+	double l38 = (l5 * l11) - (l7 * l9);
+	double l39 = (l5 * l10) - (l6 * l9);
+	matrix.m[0] = l23 * l27;
+	matrix.m[4] = l24 * l27;
+	matrix.m[8] = l25 * l27;
+	matrix.m[12] = l26 * l27;
+	matrix.m[1] = -(((l2 * l17) - (l3 * l18)) + (l4 * l19)) * l27;
+	matrix.m[5] = (((l1 * l17) - (l3 * l20)) + (l4 * l21)) * l27;
+	matrix.m[9] = -(((l1 * l18) - (l2 * l20)) + (l4 * l22)) * l27;
+	matrix.m[13] = (((l1 * l19) - (l2 * l21)) + (l3 * l22)) * l27;
+	matrix.m[2] = (((l2 * l28) - (l3 * l29)) + (l4 * l30)) * l27;
+	matrix.m[6] = -(((l1 * l28) - (l3 * l31)) + (l4 * l32)) * l27;
+	matrix.m[10] = (((l1 * l29) - (l2 * l31)) + (l4 * l33)) * l27;
+	matrix.m[14] = -(((l1 * l30) - (l2 * l32)) + (l3 * l33)) * l27;
+	matrix.m[3] = -(((l2 * l34) - (l3 * l35)) + (l4 * l36)) * l27;
+	matrix.m[7] = (((l1 * l34) - (l3 * l37)) + (l4 * l38)) * l27;
+	matrix.m[11] = -(((l1 * l35) - (l2 * l37)) + (l4 * l39)) * l27;
+	matrix.m[15] = (((l1 * l36) - (l2 * l38)) + (l3 * l39)) * l27;
+	return (matrix);
+}
+
+tmatrix		multiplyMarix(tmatrix matrix, tmatrix other)
+{
+	tmatrix		result;
+
+	result.m[0] = matrix.m[0] * other.m[0] + matrix.m[1] * other.m[4] + matrix.m[2] * other.m[8] + matrix.m[3] * other.m[12];
+	result.m[1] = matrix.m[0] * other.m[1] + matrix.m[1] * other.m[5] + matrix.m[2] * other.m[9] + matrix.m[3] * other.m[13];
+	result.m[2] = matrix.m[0] * other.m[2] + matrix.m[1] * other.m[6] + matrix.m[2] * other.m[10] + matrix.m[3] * other.m[14];
+	result.m[3] = matrix.m[0] * other.m[3] + matrix.m[1] * other.m[7] + matrix.m[2] * other.m[11] + matrix.m[3] * other.m[15];
+	result.m[4] = matrix.m[4] * other.m[0] + matrix.m[5] * other.m[4] + matrix.m[6] * other.m[8] + matrix.m[7] * other.m[12];
+	result.m[5] = matrix.m[4] * other.m[1] + matrix.m[5] * other.m[5] + matrix.m[6] * other.m[9] + matrix.m[7] * other.m[13];
+	result.m[6] = matrix.m[4] * other.m[2] + matrix.m[5] * other.m[6] + matrix.m[6] * other.m[10] + matrix.m[7] * other.m[14];
+	result.m[7] = matrix.m[4] * other.m[3] + matrix.m[5] * other.m[7] + matrix.m[6] * other.m[11] + matrix.m[7] * other.m[15];
+	result.m[8] = matrix.m[8] * other.m[0] + matrix.m[9] * other.m[4] + matrix.m[10] * other.m[8] + matrix.m[11] * other.m[12];
+	result.m[9] = matrix.m[8] * other.m[1] + matrix.m[9] * other.m[5] + matrix.m[10] * other.m[9] + matrix.m[11] * other.m[13];
+	result.m[10] = matrix.m[8] * other.m[2] + matrix.m[9] * other.m[6] + matrix.m[10] * other.m[10] + matrix.m[11] * other.m[14];
+	result.m[11] = matrix.m[8] * other.m[3] + matrix.m[9] * other.m[7] + matrix.m[10] * other.m[11] + matrix.m[11] * other.m[15];
+	result.m[12] = matrix.m[12] * other.m[0] + matrix.m[13] * other.m[4] + matrix.m[14] * other.m[8] + matrix.m[15] * other.m[12];
+	result.m[13] = matrix.m[12] * other.m[1] + matrix.m[13] * other.m[5] + matrix.m[14] * other.m[9] + matrix.m[15] * other.m[13];
+	result.m[14] = matrix.m[12] * other.m[2] + matrix.m[13] * other.m[6] + matrix.m[14] * other.m[10] + matrix.m[15] * other.m[14];
+	result.m[15] = matrix.m[12] * other.m[3] + matrix.m[13] * other.m[7] + matrix.m[14] * other.m[11] + matrix.m[15] * other.m[15];
+	return (result);
+}
+
 void render(tdevice device, tcam camera, tmesh meshes)
 {
-/*
-ViewMatrix viewMatrix = MatrixLookAtLH(camera.Position, camera.Target, Vector3Up());
-	ProjectionMatrix projectionMatrix = MatrixPerspectiveFovLH(0.78, workingWidth / workingHeight, 0.01, 1);
-	for (int index = 0, index < meshes.length, index++)
-	{
-		tmesh cMesh = meshes[index];
-		WorldMatrix worldMatrix = MatrixRotationYawPitchRoll(cMesh.Rotation.y,cMesh.Rotation.x, cMesh.Rotation.z)
-	+++
-	TransformMatrix TransformMatrix(worldMatrix
-	}
-*/
+	/*
+	   ViewMatrix viewMatrix = MatrixLookAtLH(camera.Position, camera.Target, Vector3Up());
+	   ProjectionMatrix projectionMatrix = MatrixPerspectiveFovLH(0.78, workingWidth / workingHeight, 0.01, 1);
+	   for (int index = 0, index < meshes.length, index++)
+	   {
+	   tmesh cMesh = meshes[index];
+	   WorldMatrix worldMatrix = MatrixRotationYawPitchRoll(cMesh.Rotation.y,cMesh.Rotation.x, cMesh.Rotation.z)
+	   +++
+	   TransformMatrix TransformMatrix(worldMatrix
+	   }
+	   */
 }
